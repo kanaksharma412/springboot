113e37d8e64af3c9f726b4036c63e5f57a16f4db
kanaksharma412 <kanaksharma412@gwu.edu>
2020-10-07 18:12:20 -0400

    Added a few missing files

---
+++ target/classes/data.sql
@@ -0,0 +1,4 @@
+insert into Userspring values (1,'12/04/1995','kanaksharma412@gmail.com','Kanak','Male','Sharma','Rooney123!');
+insert into Userspring values (2,'12/05/1996','kanaksharma412@gwu.edu','Rajat','Male','Sharma','Rooney12312!');
+insert into Userspring values (3,'12/06/1997','kanaksharma410@gmail.com','John','Male','Cash','Rooney12355!');
+insert into Userspring values (4,'12/07/1995','kanaksharma411@gmail.com','Luke','Male','Smith','Rooney123678!');
